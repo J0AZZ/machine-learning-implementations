a4e46db0d90668beedcf0ddd308fb8d54085d817
britof <41692419+britof@users.noreply.github.com>
2020-09-10 12:05:32 -0300

    K Nearest Neighbors
    Performs KNN using Manhattan Distance (L1 Norm) as standard vector distance measurement.

--- Python/KNearestNeighbors.py
+++ Python/KNearestNeighbors.py
@@ -7,22 +7,6 @@ Created on Wed Sep  9 19:04:43 2020
 @author: Joás de Brito Ferreira Filho
 """
 
-from math import sqrt
-import heapq
-
-# For two features (2-dimensional observations)
-
-## EUCLIDIAN DISTANCE (L2 Norm)
-def distance_2d(p1, p2):
-    return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
-
-# Measures the euclidian distance from point to all points in plane (for 2-dimensional vectors)
-# and returns the k nearest neighbors with respect to a point
-def knn_2d(point, plane, k=1):
-    distances = list()
-    for p in plane:
-        distances.append(distance_2d(point.coordinates, p.coordinates))
-    return heapq.nsmallest(k, distances)
 
 ## MANHATTAN DISTANCE (L1 Norm)
 def manhattan_distance(p1, p2):
@@ -36,8 +20,11 @@ def manhattan_distance(p1, p2):
 def knn(point, plane, k):
     distances = list()
     for p in plane:
-        distances.append(manhattan_distance(point.coordinates, p.coordinates))
-    return heapq.nsmallest(k, distances)
+        distances.append([p, manhattan_distance(point.coordinates, p.coordinates)])
+    index = distances.argsort()[:k]
+    knn = list()
+    for i in index:
+        knn.append(plane[i])
 
 
 
