c1ad7dcb70f1e7e70f03663931b26d225a208760
britof <41692419+britof@users.noreply.github.com>
2020-09-11 20:23:53 -0300

    Update KMeansClustering.py
    Needs a convergence condition to stop the main loop.

--- Python/KMeansClustering.py
+++ Python/KMeansClustering.py
@@ -16,26 +16,42 @@ def manhattan_distance(p1, p2):
         d.append(abs(p1[i]-p2[i]))
     return d
 
+# Averages the vectors' coordinates to get the centroid
 def get_centroid(vectors):
     centroid = list()
-    for v in vectors:
-        for dimensions in v:
-            
+    for c in range(len(vectors[0])):
+        coord = 0
+        for v in vectors:
+            coord += v[c]
+        centroid.append(coord/len(vectors))
+    return centroid
 
 # Returns k clusters containing the same vectors passed as argument
 def kmc(vectors, k):
     # k randomly selected vectors, to be used in the first iteration
-    C = random.sample(vectors, k)
+    centroids = random.sample(vectors, k)
     
+    # list to hold k clusters
     clusters = list()
     for n in range(k):
         clusters.append(list())
-        
-    for v in vectors:
-        distance_to_centroid_k = list()
-        for c in C:
-            distance_to_centroid_k.append(manhattan_distance(c, v))
-        c_index = distance_to_centroid_k.argsort()[:1]
-        clusters[c_index].append(v)
-        
+    
+    converged = False
+    # repeat until convergence
+    while(!converged):
+        # for each vector determine to which centroid it is closer
+        for v in vectors:
+            distance_to_centroid_k = list()
+            for k in centroids:
+                distance_to_centroid_k.append(manhattan_distance(k, v))
+                
+            # index of the centroid k closest to vector v
+            K = distance_to_centroid_k.argsort()[:1]
+            # include v to the apropriate cluster
+            clusters[K].append(v)
+    
+        # calculate new centroids
+        centroids = list()
+        for cluster in clusters:
+            centroids.append(get_centroid(cluster))
             
