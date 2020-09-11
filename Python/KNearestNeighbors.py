3cf77bef5e3cf227a8ecdbb3c0cecdc0fbab60c2
britof <41692419+britof@users.noreply.github.com>
2020-09-11 14:11:15 -0300

    Update KNearestNeighbors.py

--- Python/KNearestNeighbors.py
+++ Python/KNearestNeighbors.py
@@ -7,6 +7,8 @@ Created on Wed Sep  9 19:04:43 2020
 @author: Joás de Brito Ferreira Filho
 """
 
+from statistics import mode
+
 
 ## MANHATTAN DISTANCE (L1 Norm)
 def manhattan_distance(p1, p2):
@@ -16,8 +18,9 @@ def manhattan_distance(p1, p2):
     return distance
 
 # Measures the Manhattan Distance from point to all points in plane 
-# and returns the k smallest distances
-def knn(point, plane, k):
+# and returns the k points with smallest distances to point.
+# if predict is set to True, returns the label predicted by the KNN
+def knn(point, plane, k, predict=False):
     distances = list()
     for p in plane:
         distances.append([p, manhattan_distance(point.coordinates, p.coordinates)])
@@ -25,6 +28,12 @@ def knn(point, plane, k):
     knn = list()
     for i in index:
         knn.append(plane[i])
-
+    if(!predict):
+        return knn
+    else:
+        labels = list()
+        for p in knn:
+            labels.append(p.label)
+        return mode(labels)
 
 
